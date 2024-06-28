2f5959fa4609a80d893f2cc43e059345d5d5d304
Brett Sutton <bsutton@noojee.com.au>
2024-06-28 22:07:55 +1000

    stopped time_entries being edited/deleted after they have been invoiced.

--- lib/crud/time_entry/time_entry_list_screen.dart
+++ lib/crud/time_entry/time_entry_list_screen.dart
@@ -4,6 +4,7 @@ import '../../dao/dao_time_entry.dart';
 import '../../entity/task.dart';
 import '../../entity/time_entry.dart';
 import '../../util/format.dart';
+import '../../widgets/hmb_row_gap.dart';
 import '../base_nested/nested_list_screen.dart';
 import 'time_entry_edit_screen.dart';
 
@@ -22,14 +23,17 @@ class TimeEntryListScreen extends StatelessWidget {
       // ignore: discarded_futures
       fetchList: () => DaoTimeEntry().getByTask(parent.parent?.id),
       title: (entity) => Text(
-          '''Interval: ${formatDateTime(entity.startTime)} - ${entity.endTime != null ? formatDateTime(entity.endTime!) : 'running'}'''),
+          '''Interval: ${formatDateTime(entity.startTime)} - ${entity.endTime != null ? formatDateTime(entity.endTime!) : 'running'}; ${entity.note}'''),
       onEdit: (timeEntry) =>
           TimeEntryEditScreen(task: parent.parent!, timeEntry: timeEntry),
+      canEdit: (timeEntry) => timeEntry.billed == false,
       onDelete: (timeEntry) async => DaoTimeEntry().delete(timeEntry!.id),
+      canDelete: (timeEntry) => timeEntry.billed == false,
       onInsert: (timeEntry) async => DaoTimeEntry().insert(timeEntry!),
       details: (timeEntry, details) => Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              Text('Billed: ${timeEntry.billed}'),
+              const HMBRowGap(),
               Text(
                   '''Duration: ${timeEntry.endTime == null ? 'running' : formatDuration(timeEntry.endTime!.difference(timeEntry.startTime))}'''),
             ],
