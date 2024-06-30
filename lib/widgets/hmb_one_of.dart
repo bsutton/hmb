6facef88ff6a55ede9a1cbe1b0f10909cbc0e6e2
Brett Sutton <bsutton@onepub.dev>
2024-06-30 11:07:17 +1000

    impoved the money fields validation - it wasn't working.

---
+++ lib/widgets/hmb_one_of.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+/// Display one or another widget based on a condition.
+/// If [condition] is true, [onTrue] is displayed,
+///  otherwise [onFalse] is displayed.
+class HMBOneOf extends StatelessWidget {
+  const HMBOneOf(
+      {required this.condition,
+      required this.onTrue,
+      required this.onFalse,
+      super.key});
+
+  final bool condition;
+  final Widget onTrue;
+  final Widget onFalse;
+
+  @override
+  Widget build(BuildContext context) => condition ? onTrue : onFalse;
+}
