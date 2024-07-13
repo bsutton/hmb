3a512d1fb9670140f4f6d91194649bf971b42074
Brett Sutton <bsutton@noojee.com.au>
2024-07-14 08:32:00 +1000

    added .well-know to the packing list so assetlinks.json is shipped. updated build.gradel version number to publish to google play console.

--- tool/gen_sha_fingerprint.dart
+++ tool/gen_sha_fingerprint.dart
@@ -8,7 +8,8 @@ import 'keystore.dart';
 /// Generates an sha fingerprint for the apps certificate.
 /// This is used by the .well-known/assetslinks.json for deeplink
 /// valiation amongst other possible uses.
-///
+/// Each time we do a release we need to add the apps signature
+/// to deep links.
 void main() {
   'keytool -list -v -keystore $keyStorePath -alias $keyStoreAlias'.run;
 }
