4cda85238814933af39d24624199cb87e3741c9f
Brett Sutton <bsutton@noojee.com.au>
2024-07-14 11:28:44 +1000

    deployed an .well-known/assetlinks.json file to the ihs server so we can use it to validate deeplinks to hmb.invanhoehandyman.com.au updated the reboot command

--- tool/gen_sha_fingerprint.dart
+++ tool/gen_sha_fingerprint.dart
@@ -11,5 +11,11 @@ import 'keystore.dart';
 /// Each time we do a release we need to add the apps signature
 /// to deep links.
 void main() {
+  print(orange('Generating release sha256 key'));
   'keytool -list -v -keystore $keyStorePath -alias $keyStoreAlias'.run;
+
+  print(orange('Generating debug sha256 key'));
+  'keytool -list -v -keystore $keyStorePathForDebug -alias $keyStoreAliasForDebug'.run;
+
+  print(green('Check that the ihserver/www_root/.well-known/assetlinks.json file has both of this signatures'));
 }
