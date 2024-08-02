1a4b44bfc1e2c79c32335881db0310601f401e79
Brett Sutton <bsutton@noojee.com.au>
2024-08-02 15:54:26 +1000

    Added taskname and photo comment to the full screen photo view.

--- lib/widgets/full_screen_photo_view.dart
+++ lib/widgets/full_screen_photo_view.dart
@@ -4,8 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
 
 class FullScreenPhotoViewer extends StatelessWidget {
-  const FullScreenPhotoViewer({required this.imagePath, super.key});
+  const FullScreenPhotoViewer({
+    required this.imagePath,
+    required this.taskName,
+    required this.comment,
+    super.key,
+  });
+
   final String imagePath;
+  final String taskName;
+  final String comment;
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -15,6 +23,36 @@ class FullScreenPhotoViewer extends StatelessWidget {
             PhotoView(
               imageProvider: FileImage(File(imagePath)),
               backgroundDecoration: const BoxDecoration(color: Colors.black),
+              minScale: PhotoViewComputedScale.contained,
+              maxScale: PhotoViewComputedScale.covered * 2.0,
+              initialScale: PhotoViewComputedScale.contained,
+            ),
+            Positioned(
+              top: 40,
+              left: 20,
+              right: 20,
+              child: Column(
+                children: [
+                  Text(
+                    taskName,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  if (comment.isNotEmpty)
+                    Text(
+                      comment,
+                      style: const TextStyle(
+                        color: Colors.white70,
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                ],
+              ),
             ),
             Positioned(
               top: 40,
