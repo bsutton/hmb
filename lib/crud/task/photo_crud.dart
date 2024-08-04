51b4529ec0af04d1ec3024bf73bcd638bedd4cd2
Brett Sutton <bsutton@noojee.com.au>
2024-08-04 22:05:57 +1000

    refactored the photo_crud into its own widget. But now trying to delete a photo causes the app to lock up.

---
+++ lib/crud/task/photo_crud.dart
@@ -0,0 +1,280 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:future_builder_ex/future_builder_ex.dart';
+import 'package:go_router/go_router.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:june/june.dart';
+import 'package:path_provider/path_provider.dart';
+
+import '../../dao/dao_photo.dart';
+import '../../dao/dao_task.dart';
+import '../../entity/photo.dart';
+import '../../entity/task.dart';
+import '../../widgets/photo_gallery.dart';
+
+class PhotoCrud extends StatefulWidget {
+  const PhotoCrud({required this.controller, super.key});
+
+  final PhotoController controller;
+
+  @override
+  State<PhotoCrud> createState() => _PhotoCrudState();
+}
+
+class _PhotoCrudState extends State<PhotoCrud> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.controller._task == null) {
+      return const Center(child: Text('To Add a Photo - Save the Task First'));
+    }
+    // Display photos and allow adding comments and deletion
+    else {
+      return JuneBuilder(PhotoLoader.new,
+          builder: (context) => FutureBuilderEx(
+              // ignore: discarded_futures
+              future: widget.controller.photos,
+              builder: (context, photos) => Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      _buildAddButton(widget.controller._task, photos),
+                      _buildPhotoCRUD(photos),
+                    ],
+                  )));
+    }
+  }
+
+  /// Build the take photo button
+  Widget _buildAddButton(Task? task, List<Photo>? photos) => IconButton(
+        icon: const Icon(Icons.camera_alt),
+        onPressed: () async {
+          final photoFile = await takePhoto();
+          if (photoFile != null) {
+            // Insert the photo metadata into the database
+            final newPhoto = Photo.forInsert(
+              taskId: task!.id,
+              filePath: photoFile.path,
+              comment: '',
+            );
+            await widget.controller.addPhoto(newPhoto);
+          }
+        },
+      );
+
+  /// Build the photo CRUD
+  Widget _buildPhotoCRUD(List<Photo>? photos) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: photos!
+            .map((photo) => Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    _showPhoto(photo),
+                    _buildCommentField(photo),
+                    _buildDeleteButton(photo),
+                  ],
+                ))
+            .toList(),
+      );
+
+  /// Display the photo with a 'Icon in the corner
+  /// which when tapped will show the full screen photo
+  /// with zoom/pan ability.
+  Stack _showPhoto(Photo photo) => Stack(
+        children: [
+          Image.file(File(photo.filePath)),
+          Positioned(
+            right: 0,
+            child: GestureDetector(
+              // Show full screen photo when tapped
+              onTap: () async {
+                final task = await DaoTask().getById(photo.taskId);
+                if (mounted) {
+                  await _showFullScreenPhoto(
+                      context, photo.filePath, task!.name, photo.comment);
+                }
+              },
+              child: Container(
+                color: Colors.black.withOpacity(0.5),
+                padding: const EdgeInsets.all(8),
+                child: const Icon(
+                  Icons.fullscreen,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      );
+
+  IconButton _buildDeleteButton(Photo photo) => IconButton(
+        icon: const Icon(Icons.delete),
+        onPressed: () async {
+          await _showConfirmDeleteDialog(context, photo);
+          PhotoGallery.notify();
+        },
+      );
+
+  Widget _buildCommentField(Photo photo) {
+    final commentControlller = widget.controller.commentController(photo);
+    return TextField(
+      controller: commentControlller,
+      decoration: const InputDecoration(labelText: 'Comment'),
+      maxLines: null, // Allows the field to grow as needed
+      onChanged: (value) {
+        // Update comment immediately when text changes
+        photo.comment = value;
+      },
+    );
+  }
+
+  Future<void> _showConfirmDeleteDialog(
+          BuildContext context, Photo photo) async =>
+      showDialog<void>(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Delete Photo'),
+          content: const ListBody(
+            children: <Widget>[
+              // Image.file(File(photo.filePath),
+              //     width: 100, height: 100), // Thumbnail of the photo
+              // if (photo.comment.isNotEmpty) Text(photo.comment),
+              // const SizedBox(height: 10),
+              Text('Are you sure you want to delete this photo?'),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Delete'),
+              onPressed: () async {
+                await widget.controller.deletePhoto(photo);
+                if (context.mounted) {
+                  Navigator.of(context).pop();
+                }
+              },
+            ),
+          ],
+        ),
+      );
+
+  Future<void> _showFullScreenPhoto(BuildContext context, String imagePath,
+      String taskName, String comment) async {
+    await context.push('/photo_viewer', extra: {
+      'imagePath': imagePath,
+      'taskName': taskName,
+      'comment': comment,
+    });
+  }
+
+  Future<File?> takePhoto() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+    if (pickedFile == null) {
+      return null;
+    }
+
+    final appDir = await getApplicationDocumentsDirectory();
+    final fileName = pickedFile.path.split('/').last;
+    final savedImage =
+        await File(pickedFile.path).copy('${appDir.path}/$fileName');
+
+    return savedImage;
+  }
+}
+
+class PhotoLoader extends JuneState {}
+
+class PhotoController {
+  PhotoController({required Task? task}) : _task = task {
+    unawaited(_loadPhotos());
+  }
+
+  Task? _task;
+  final List<Photo> _photos = [];
+
+  Task? get task => _task;
+
+  set task(Task? task) {
+    _task = task;
+    June.getState(PhotoLoader.new).setState();
+  }
+
+  Future<List<Photo>> get photos async {
+    await _completer.future;
+    return _photos;
+  }
+
+  final Completer<void> _completer = Completer<void>();
+
+  // List to hold comment controllers for each photo
+  final List<TextEditingController> _commentControllers = [];
+
+  Future<void> _loadPhotos() async {
+    if (_task == null) {
+      _completer.complete();
+      return;
+    }
+    _photos.addAll(await DaoPhoto().getByTask(_task!.id));
+
+    // Initialize comment controllers if not already initialized
+    if (_commentControllers.isEmpty) {
+      for (final photo in _photos) {
+        final controller = TextEditingController(text: photo.comment);
+
+        _commentControllers.add(controller);
+      }
+    }
+    _completer.complete();
+  }
+
+  /// Save comments explicitly when saving the task
+  Future<void> save() async {
+    await _completer.future;
+    for (var i = 0; i < _commentControllers.length; i++) {
+      final photo = _photos[i]..comment = _commentControllers[i].text;
+      await DaoPhoto().update(photo);
+    }
+    PhotoGallery.notify();
+  }
+
+  void dispose() {
+    for (final controller in _commentControllers) {
+      controller.dispose();
+    }
+  }
+
+  TextEditingController commentController(Photo photo) =>
+      _commentControllers[_photos.indexOf(photo)];
+
+  Future<void> addPhoto(Photo newPhoto) async {
+    await DaoPhoto().insert(newPhoto);
+
+    _photos.add(newPhoto);
+    _commentControllers.add(TextEditingController());
+    _refresh();
+  }
+
+  Future<void> deletePhoto(Photo photo) async {
+    // Delete the photo from the database and the disk
+    await DaoPhoto().delete(photo.id);
+    await File(photo.filePath).delete();
+
+    _refresh();
+  }
+
+  void _refresh() {
+    June.getState(PhotoLoader.new).setState();
+    PhotoGallery.notify();
+  }
+}
--- lib/crud/task/task_edit_screen.dart
+++ lib/crud/task/task_edit_screen.dart
@@ -1,22 +1,16 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:future_builder_ex/future_builder_ex.dart';
-import 'package:go_router/go_router.dart'; // Import GoRouter
-import 'package:image_picker/image_picker.dart';
 import 'package:june/june.dart';
-import 'package:path_provider/path_provider.dart';
 
 import '../../dao/dao_checklist.dart';
-import '../../dao/dao_photo.dart'; // Import the Photo DAO
 import '../../dao/dao_task.dart';
 import '../../dao/dao_task_status.dart';
 import '../../dao/join_adaptors/join_adaptor_check_list_item.dart';
 import '../../entity/check_list.dart';
 import '../../entity/job.dart';
-import '../../entity/photo.dart'; // Import the Photo entity
 import '../../entity/task.dart';
 import '../../entity/task_status.dart';
 import '../../util/fixed_ex.dart';
@@ -27,9 +21,9 @@ import '../../widgets/hmb_crud_time_entry.dart';
 import '../../widgets/hmb_droplist.dart';
 import '../../widgets/hmb_text_area.dart';
 import '../../widgets/hmb_text_field.dart';
-import '../../widgets/photo_gallery.dart';
 import '../base_nested/nested_edit_screen.dart';
 import '../base_nested/nested_list_screen.dart';
+import 'photo_crud.dart';
 
 class TaskEditScreen extends StatefulWidget {
   const TaskEditScreen({required this.job, super.key, this.task});
@@ -52,13 +46,13 @@ class _TaskEditScreenState extends State<TaskEditScreen>
   late TextEditingController _descriptionController;
   late TextEditingController _estimatedCostController;
   late TextEditingController _effortInHoursController;
+  late PhotoController _photoController;
   late FocusNode _summaryFocusNode;
   late FocusNode _descriptionFocusNode;
   late FocusNode _costFocusNode;
   late FocusNode _estimatedCostFocusNode;
   late FocusNode _effortInHoursFocusNode;
   late FocusNode _itemTypeIdFocusNode;
-  late Future<List<Photo>> _photosFuture; // Future to load photos
 
   @override
   void initState() {
@@ -71,6 +65,8 @@ class _TaskEditScreenState extends State<TaskEditScreen>
         TextEditingController(text: widget.task?.estimatedCost.toString());
     _effortInHoursController =
         TextEditingController(text: widget.task?.effortInHours.toString());
+
+    _photoController = PhotoController(task: widget.task);
 
     _summaryFocusNode = FocusNode();
     _descriptionFocusNode = FocusNode();
@@ -88,17 +84,6 @@ class _TaskEditScreenState extends State<TaskEditScreen>
         FocusScope.of(context).requestFocus(_summaryFocusNode);
       });
     }
-
-    // Load photos associated with the task
-    // ignore: discarded_futures
-    _photosFuture = _loadPhotos();
-  }
-
-  Future<List<Photo>> _loadPhotos() async {
-    if (widget.task == null) {
-      return [];
-    }
-    return DaoPhoto().getByTask(widget.task!.id);
   }
 
   @override
@@ -108,6 +93,7 @@ class _TaskEditScreenState extends State<TaskEditScreen>
     _descriptionController.dispose();
     _estimatedCostController.dispose();
     _effortInHoursController.dispose();
+    _photoController.dispose();
     _summaryFocusNode.dispose();
     _descriptionFocusNode.dispose();
     _costFocusNode.dispose();
@@ -116,31 +102,16 @@ class _TaskEditScreenState extends State<TaskEditScreen>
     _itemTypeIdFocusNode.dispose();
   }
 
-  Future<File?> takePhoto() async {
-    final picker = ImagePicker();
-    final pickedFile = await picker.pickImage(source: ImageSource.camera);
-    if (pickedFile == null) {
-      return null;
-    }
-
-    final appDir = await getApplicationDocumentsDirectory();
-    final fileName = pickedFile.path.split('/').last;
-    final savedImage =
-        await File(pickedFile.path).copy('${appDir.path}/$fileName');
-
-    return savedImage;
-  }
-
-
   @override
   Widget build(BuildContext context) => NestedEntityEditScreen<Task, Job>(
         entity: widget.task,
         entityName: 'Task',
         dao: DaoTask(),
-        onInsert: (task) async => _insertTaskWithCheckList(task!),
+        onInsert: (task) async {
+          await _insertTask(task!);
+        },
         entityState: this,
         editor: (task) => Column(
-          
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -177,14 +148,7 @@ class _TaskEditScreenState extends State<TaskEditScreen>
               parent: Parent(task),
             ),
 
-            // Display photos and allow adding comments and deletion
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                _buildTakePhotoButton(task),
-                _buildPhotoCRUD(),
-              ],
-            ),
+            PhotoCrud(controller: _photoController),
           ],
         ),
       );
@@ -207,104 +171,6 @@ class _TaskEditScreenState extends State<TaskEditScreen>
                 ),
               ));
 
-  /// Build the take photo button
-  Widget _buildTakePhotoButton(Task? task) => IconButton(
-        icon: const Icon(Icons.camera_alt),
-        onPressed: () async {
-          final photoFile = await takePhoto();
-          if (photoFile != null) {
-            // Insert the photo metadata into the database
-            final newPhoto = Photo.forInsert(
-              taskId: task!.id,
-              filePath: photoFile.path,
-              comment: '',
-            );
-            await DaoPhoto().insert(newPhoto);
-            setState(() {
-              _photosFuture = _loadPhotos(); // Refresh the photos
-            });
-            PhotoGallery.notify();
-          }
-        },
-      );
-
-  /// Build the photo CRUD
-  Widget _buildPhotoCRUD() => FutureBuilderEx<List<Photo>>(
-      future: _photosFuture,
-      builder: (context, photos) => Column(
-        mainAxisSize: MainAxisSize.min,
-            children: photos!
-                .map((photo) => Column(
-                  mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Stack(
-                          children: [
-                            Image.file(File(photo.filePath)),
-                            Positioned(
-                              right: 0,
-                              child: GestureDetector(
-                                onTap: () async {
-                                  // Fetch the task for this photo to get
-                                  // the task name.
-                                  final task =
-                                      await DaoTask().getById(photo.taskId);
-                                  if (context.mounted) {
-                                    await _showFullScreenPhoto(
-                                        context,
-                                        photo.filePath,
-                                        task!.name,
-                                        photo.comment);
-                                  }
-                                },
-                                child: Container(
-                                  color: Colors.black.withOpacity(0.5),
-                                  padding: const EdgeInsets.all(8),
-                                  child: const Icon(
-                                    Icons.fullscreen,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        _buildCommentField(photo),
-                        IconButton(
-                          icon: const Icon(Icons.delete),
-                          onPressed: () async {
-                            await _showConfirmDeleteDialog(context, photo);
-                            PhotoGallery.notify();
-                          },
-                        ),
-                      ],
-                    ))
-                .toList(),
-          ));
-
-  Widget _buildCommentField(Photo photo) {
-    final _commentController = TextEditingController(text: photo.comment);
-    final _commentFocusNode = FocusNode();
-
-    _commentFocusNode.addListener(() async {
-      if (!_commentFocusNode.hasFocus) {
-        // Update the comment in the database when the text field loses focus
-        photo.comment = _commentController.text;
-        await DaoPhoto().update(photo);
-        setState(() {
-          // Photo comment updated in the database
-        });
-        PhotoGallery.notify();
-      }
-    });
-
-    return TextField(
-      controller: _commentController,
-      focusNode: _commentFocusNode,
-      decoration: const InputDecoration(labelText: 'Comment'),
-      maxLines: null, // Allows the field to grow as needed
-    );
-  }
-
   Widget _chooseTaskStatus(Task? task) => HMBDroplist<TaskStatus>(
       title: 'Task Status',
       initialItem: () async => DaoTaskStatus().getById(task?.taskStatusId ?? 1),
@@ -314,89 +180,44 @@ class _TaskEditScreenState extends State<TaskEditScreen>
         June.getState(TaskStatusState.new).taskStatusId = item.id;
       });
 
-  Future<void> _insertTaskWithCheckList(Task task) async {
+  Future<void> _insertTask(Task task) async {
     await DaoTask().insert(task);
     final newChecklist = CheckList.forInsert(
         name: 'default',
         description: 'Default Checklist',
         listType: CheckListType.owned);
     await DaoCheckList().insertForTask(newChecklist, task);
+
+    _photoController.task = task;
   }
 
   @override
-  Future<Task> forUpdate(Task task) async => Task.forUpdate(
+  Future<Task> forUpdate(Task task) async {
+    await _photoController.save();
+    return Task.forUpdate(
       entity: task,
       jobId: widget.job.id,
       name: _nameController.text,
       description: _descriptionController.text,
       estimatedCost: MoneyEx.tryParse(_estimatedCostController.text),
       effortInHours: FixedEx.tryParse(_effortInHoursController.text),
-      taskStatusId: June.getState(TaskStatusState.new).taskStatusId!);
+      taskStatusId: June.getState(TaskStatusState.new).taskStatusId!,
+    );
+  }
 
   @override
   Future<Task> forInsert() async => Task.forInsert(
-      jobId: widget.job.id,
-      name: _nameController.text,
-      description: _descriptionController.text,
-      estimatedCost: MoneyEx.tryParse(_estimatedCostController.text),
-      effortInHours: FixedEx.tryParse(_effortInHoursController.text),
-      taskStatusId: June.getState(TaskStatusState.new).taskStatusId!);
+        jobId: widget.job.id,
+        name: _nameController.text,
+        description: _descriptionController.text,
+        estimatedCost: MoneyEx.tryParse(_estimatedCostController.text),
+        effortInHours: FixedEx.tryParse(_effortInHoursController.text),
+        taskStatusId: June.getState(TaskStatusState.new).taskStatusId!,
+      );
 
   @override
   void refresh() {
-    // ignore: discarded_futures
-    _photosFuture = _loadPhotos(); // Refresh photos when screen is refreshed
     setState(() {});
-  }
-
-
-  Future<void> _showConfirmDeleteDialog(
-          BuildContext context, Photo photo) async =>
-      showDialog<void>(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: const Text('Delete Photo'),
-          content: ListBody(
-            children: <Widget>[
-              Image.file(File(photo.filePath),
-                  width: 100, height: 100), // Thumbnail of the photo
-              if (photo.comment.isNotEmpty) Text(photo.comment),
-              const SizedBox(height: 10),
-              const Text('Are you sure you want to delete this photo?'),
-            ],
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: const Text('Delete'),
-              onPressed: () async {
-                // Delete the photo from the database and the disk
-                await DaoPhoto().delete(photo.id);
-                await File(photo.filePath).delete();
-                setState(() {
-                  _photosFuture = _loadPhotos(); // Refresh the photos
-                });
-                if (context.mounted) {
-                  Navigator.of(context).pop();
-                }
-              },
-            ),
-          ],
-        ),
-      );
-
-  Future<void> _showFullScreenPhoto(BuildContext context, String imagePath,
-      String taskName, String comment) async {
-    await context.push('/photo_viewer', extra: {
-      'imagePath': imagePath,
-      'taskName': taskName,
-      'comment': comment,
-    });
   }
 }
 
