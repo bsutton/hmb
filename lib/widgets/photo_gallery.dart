33151f5638dbc97ec7fcfa67ef1ec417e75a1692
Brett Sutton <bsutton@noojee.com.au>
2024-08-28 08:22:53 +1000

    Added logic to check if the photo exists on disk when building the gallery.

--- lib/widgets/photo_gallery.dart
+++ lib/widgets/photo_gallery.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:future_builder_ex/future_builder_ex.dart';
-import 'package:go_router/go_router.dart'; //
+import 'package:go_router/go_router.dart';
 import 'package:june/june.dart';
 
 import '../dao/dao_photo.dart';
@@ -31,47 +31,64 @@ class PhotoGallery extends StatelessWidget {
             }
           }));
 
-  SizedBox buildGallery(List<Photo>? photos, BuildContext context) => SizedBox(
+  SizedBox buildGallery(List<Photo> photos, BuildContext context) => SizedBox(
         height: 100,
         child: ListView(
           scrollDirection: Axis.horizontal,
-          children: photos!
-              .map((photo) => Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: GestureDetector(
-                      onTap: () async {
-                        // Fetch the task for this photo to get
-                        // the task name.
-                        final task = await DaoTask().getById(photo.taskId);
-                        if (context.mounted) {
-                          await _showFullScreenPhoto(context, photo.filePath,
-                              task!.name, photo.comment);
-                        }
-                      },
-                      child: Stack(
-                        children: [
-                          Image.file(
-                            File(photo.filePath),
-                            width: 80,
-                            height: 80,
-                            fit: BoxFit.cover,
-                          ),
-                          const Positioned(
-                            bottom: 8,
-                            right: 0,
-                            child: ColoredBox(
-                              color: Colors.black45,
-                              child: Icon(
-                                Icons.zoom_out_map,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ],
+          children: photos.map((photo) {
+            final file = File(photo.filePath);
+            final isFileExist = file.existsSync();
+
+            return Padding(
+              padding: const EdgeInsets.all(8),
+              child: GestureDetector(
+                onTap: () async {
+                  if (isFileExist) {
+                    // Fetch the task for this photo to get the task name.
+                    final task = await DaoTask().getById(photo.taskId);
+                    if (context.mounted) {
+                      await _showFullScreenPhoto(
+                          context, photo.filePath, task!.name, photo.comment);
+                    }
+                  }
+                },
+                child: Stack(
+                  children: [
+                    if (isFileExist)
+                      Image.file(
+                        file,
+                        width: 80,
+                        height: 80,
+                        fit: BoxFit.cover,
+                      )
+                    else
+                      Container(
+                        width: 80,
+                        height: 80,
+                        color: Colors.grey,
+                        child: const Icon(
+                          Icons.broken_image,
+                          color: Colors.white,
+                          size: 40,
+                        ),
                       ),
-                    ),
-                  ))
-              .toList(),
+                    if (isFileExist)
+                      const Positioned(
+                        bottom: 8,
+                        right: 0,
+                        child: ColoredBox(
+                          color: Colors.black45,
+                          child: Icon(
+                            Icons.zoom_out_map,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            );
+          }).toList(),
         ),
       );
 
