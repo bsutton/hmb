c70d95bc9724de1283b1af77cd595ce8583b1ce5
Brett Sutton <bsutton@noojee.com.au>
2024-09-09 09:28:24 +1000

    added copy to clipboard option for addresses.

--- lib/widgets/hmb_map_icon.dart
+++ lib/widgets/hmb_map_icon.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:strings/strings.dart';
 
 import '../entity/site.dart';
+import '../util/clip_board.dart';
 import '../util/google_maps.dart';
 
 class HMBMapIcon extends StatelessWidget {
@@ -8,21 +10,39 @@ class HMBMapIcon extends StatelessWidget {
   final Site? site;
 
   @override
-  Widget build(BuildContext context) => Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween, // added line
-        mainAxisSize: MainAxisSize.min, // added
-        children: [
-          IconButton(
-            padding: EdgeInsets.zero,
-            alignment: Alignment.centerLeft,
-            visualDensity: VisualDensity.compact,
-            iconSize: 25,
-            icon: const Icon(Icons.map),
-            onPressed: () async =>
-                site == null ? null : GoogleMaps.openMap(context, site!),
-            color: site != null && !site!.isEmpty() ? Colors.blue : Colors.grey,
-            tooltip: 'Get Directions',
-          ),
-        ],
-      );
+  Widget build(BuildContext context) {
+    final address = Strings.join([
+      site?.addressLine1,
+      site?.addressLine2,
+      site?.suburb,
+      site?.state,
+      site?.postcode
+    ], separator: ', ', excludeEmpty: true);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween, // added line
+      mainAxisSize: MainAxisSize.min, // added
+      children: [
+        IconButton(
+          padding: EdgeInsets.zero,
+          alignment: Alignment.centerLeft,
+          visualDensity: VisualDensity.compact,
+          iconSize: 25,
+          icon: const Icon(Icons.map),
+          onPressed: () async =>
+              site == null ? null : GoogleMaps.openMap(context, site!),
+          color: site != null && !site!.isEmpty() ? Colors.blue : Colors.grey,
+          tooltip: 'Get Directions',
+        ),
+        IconButton(
+          iconSize: 22,
+          icon: const Icon(Icons.copy),
+          onPressed: () async => Strings.isEmpty(address)
+              ? null
+              : clipboardCopyTo(context, address),
+          color: Strings.isEmpty(address) ? Colors.grey : Colors.blue,
+          tooltip: 'Copy Address to the Clipboard',
+        ),
+      ],
+    );
+  }
 }
