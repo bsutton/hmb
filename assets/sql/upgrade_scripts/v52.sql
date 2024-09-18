ffee847138a499bd3c89177089477ec6f00c6308
Brett Sutton <bsutton@noojee.com.au>
2024-09-19 07:25:12 +1000

    renamed checklist item columns to be more evocative of their usage. Added estimated_labour_cost as a column.

--- assets/sql/upgrade_scripts/v52.sql
+++ assets/sql/upgrade_scripts/v52.sql
@@ -1,7 +1,9 @@
 ALTER TABLE check_list_item RENAME COLUMN estimated_material_cost TO estimated_material_unit_cost;
 
 ALTER TABLE check_list_item RENAME COLUMN estimated_labour TO estimated_labour_hours;
-ALTER TABLE check_list_item RENAME COLUMN estimated_cost TO estimated_labour_cost;
+
+ALTER TABLE check_list_item ADD COLUMN estimated_labour_cost INTEGER;
+
 
 
 
