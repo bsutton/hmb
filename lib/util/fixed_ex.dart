9841495d1a52c49f964eb4f53acb2de7d5545b43
Brett Sutton <bsutton@onepub.dev>
2024-09-18 20:11:45 +1000

    changed FixedEx to always use a scale 3 as that is our default across the app.

--- lib/util/fixed_ex.dart
+++ lib/util/fixed_ex.dart
@@ -5,18 +5,16 @@ extension FixedEx on Fixed {
   static Fixed get zero => Fixed.fromInt(0);
 
   static Fixed tryParse(String? amount) =>
-      Fixed.tryParse(Strings.orElseOnBlank(amount, '0')) ?? zero;
+      Fixed.tryParse(Strings.orElseOnBlank(amount, '0'), scale: 3) ?? zero;
 
   static Fixed tryParseOrElse(String? amount, Fixed orElse) {
     if (Strings.isBlank(amount)) {
       return orElse;
     }
-    return Fixed.tryParse(amount ?? '') ?? orElse;
+    return Fixed.tryParse(amount ?? '', scale: 3) ?? orElse;
   }
 
-  static Fixed fromInt(int? amount) => Fixed.fromInt(
-        amount ?? 0,
-      );
+  static Fixed fromInt(int? amount) => Fixed.fromInt(amount ?? 0, scale: 3);
 
   static bool isZeroOrNull(Fixed? amount) => amount == null || amount.isZero;
 }
