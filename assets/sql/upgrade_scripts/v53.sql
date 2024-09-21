bfa54b25a79d9864c700421fbed269b88277b5e2
Brett Sutton <bsutton@noojee.com.au>
2024-09-21 17:16:53 +1000

    missing links after earlier update didn't creat them.

--- assets/sql/upgrade_scripts/v53.sql
+++ assets/sql/upgrade_scripts/v53.sql
@@ -1,49 +1,5 @@
--- move data from task to check_list_items
--- Insert check_list_item for tasks with effort_in_hours
-INSERT INTO check_list_item (
-    check_list_id,
-    description,
-    item_type_id,
-    estimated_material_unit_cost,
-    estimated_labour_hours,
-    estimated_material_quantity,
-    completed,
-    billed,
-    createdDate,
-    modifiedDate,
-    measurement_type,
-    dimension1,
-    dimension2,
-    dimension3,
-    units,
-    url,
-    supplier_id,
-    estimated_labour_cost
-)
-SELECT 
-    cl.id,                                  -- Use existing check_list_id
-    'Action for Effort in Hours',           -- Description
-    5,                                      -- Labour item type
-    0,                                      -- No material unit cost
-    t.effort_in_hours,                      -- Labour hours from task
-    1,                                      -- Default quantity
-    0,                                      -- Not completed
-    0,                                      -- Not billed
-    datetime('now'),                        -- Created date
-    datetime('now'),                        -- Modified date
-    NULL,                                   -- Measurement type
-    0,                                      -- Dimension1
-    0,                                      -- Dimension2
-    0,                                      -- Dimension3
-    '',                                     -- Units
-    '',                                     -- URL
-    NULL,                                   -- Supplier ID
-    0                                       -- Estimated labour cost
-FROM task t
-JOIN task_check_list tcl ON t.id = tcl.task_id
-JOIN check_list cl ON tcl.check_list_id = cl.id
-WHERE t.effort_in_hours IS NOT NULL AND t.effort_in_hours > 0;
 
+-- add missing links after v50.sql didn't create them
 -- Insert into check_list_check_list_item to create a link
 INSERT INTO check_list_check_list_item (
     check_list_id,
@@ -61,51 +17,8 @@ JOIN check_list_item cli ON cl.id = cli.check_list_id
 JOIN task t ON t.id = (SELECT task_id FROM task_check_list WHERE check_list_id = cl.id LIMIT 1)
 WHERE cli.description = 'Action for Effort in Hours';
 
--- Insert check_list_item for tasks with estimated_cost
-INSERT INTO check_list_item (
-    check_list_id,
-    description,
-    item_type_id,
-    estimated_material_unit_cost,
-    estimated_labour_hours,
-    estimated_material_quantity,
-    completed,
-    billed,
-    createdDate,
-    modifiedDate,
-    measurement_type,
-    dimension1,
-    dimension2,
-    dimension3,
-    units,
-    url,
-    supplier_id,
-    estimated_labour_cost
-)
-SELECT 
-    cl.id,                                  -- Use existing check_list_id
-    'Action for Estimated Cost',            -- Description
-    5,                                      -- Labour item type
-    0,                                      -- No material unit cost
-    0,                                      -- No labour hours
-    1,                                      -- Default quantity
-    0,                                      -- Not completed
-    0,                                      -- Not billed
-    datetime('now'),                        -- Created date
-    datetime('now'),                        -- Modified date
-    NULL,                                   -- Measurement type
-    0,                                      -- Dimension1
-    0,                                      -- Dimension2
-    0,                                      -- Dimension3
-    '',                                     -- Units
-    '',                                     -- URL
-    NULL,                                   -- Supplier ID
-    t.estimated_cost                        -- Estimated cost from task
-FROM task t
-JOIN task_check_list tcl ON t.id = tcl.task_id
-JOIN check_list cl ON tcl.check_list_id = cl.id
-WHERE t.estimated_cost IS NOT NULL AND t.estimated_cost > 0;
 
+-- add missing links after v50.sql didn't create them
 -- Insert into check_list_check_list_item to create a link
 INSERT INTO check_list_check_list_item (
     check_list_id,
