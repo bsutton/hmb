0bc333a1f08a752a4d959a8512a73ff57022c254
Brett Sutton <bsutton@onepub.dev>
2024-10-06 11:31:41 +1100

    added ability to connect to  the fixure database from the sqlite_cli tool.

--- tool/sqlite_cli.dart
+++ tool/sqlite_cli.dart
@@ -1,5 +1,8 @@
 #! /home/bsutton/.dswitch/active/dart
 
+import 'dart:io';
+
+import 'package:args/args.dart';
 import 'package:dcli/dcli.dart';
 import 'package:path/path.dart';
 
@@ -7,7 +10,22 @@ import 'package:path/path.dart';
 /// dev database
 
 void main(List<String> args) async {
-  final pathToDb = join(DartProject.self.pathToDartToolDir,
-      'sqflite_common_ffi', 'databases', 'handyman.db');
+  final parser = ArgParser()
+    ..addFlag('help', abbr: 'h')
+    ..addFlag('test', abbr: 't', help: 'Connect to the test fixtures db');
+
+  final results = parser.parse(args);
+  if (results['help'] as bool || results.arguments.isEmpty) {
+    print(parser.usage);
+    exit(0);
+  }
+  var pathToDb = join(DartProject.self.pathToDartToolDir, 'sqflite_common_ffi',
+      'databases', 'handyman.db');
+
+  if (results['test'] as bool) {
+    pathToDb =
+        join(DartProject.self.pathToTestDir, 'fixture', 'db', 'handyman_test.db');
+  }
+
   'sqlite3 $pathToDb'.start(terminal: true);
 }
