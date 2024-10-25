3813dbe53fa3a71f2e7de11473d580dbc628e34c
Brett Sutton <bsutton@onepub.dev>
2024-10-26 10:23:21 +1100

    fixed bugs in the t&m invoice test.

--- test/dao/dao_invoice_test.dart
+++ test/dao/dao_invoice_test.dart
@@ -1,12 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hmb/dao/_index.g.dart';
 import 'package:hmb/entity/_index.g.dart';
-import 'package:hmb/util/measurement_type.dart';
+import 'package:hmb/util/format.dart';
+import 'package:hmb/util/local_date.dart';
 import 'package:hmb/util/money_ex.dart';
-import 'package:hmb/util/units.dart';
 import 'package:money2/money2.dart';
 
 import '../database/management/db_utility_test.dart';
+import 'invoice/utility.dart';
 
 void main() {
   setUp(() async {
@@ -20,78 +21,57 @@ void main() {
     test('''
 should create an invoice for time and materials job with correct rates and mark items as billed''',
         () async {
-      // Insert a job with time and materials billing type
-      final job = Job.forInsert(
-        customerId: 1, // Assuming a customer ID
-        summary: 'Time and Materials Job',
-        description: 'This is a T&M job',
-        startDate: DateTime.now(),
-        siteId: 1, // Assuming a site ID
-        contactId: 1, // Assuming a contact ID
-        jobStatusId: 1, // Assuming job status ID
-        hourlyRate: MoneyEx.fromInt(5000), // $50 per hour
-        callOutFee: MoneyEx.fromInt(1000), // $10 callout fee
-      );
-      final jobId = await DaoJob().insert(job);
-
-      // Insert task for the job
-      final task = Task.forInsert(
-        jobId: jobId,
-        name: 'Task 1',
-        description: 'First task for T&M',
-        taskStatusId: 1, // Assuming task status ID
-      );
-      final taskId = await DaoTask().insert(task);
-
-      // Insert a time entry for the task
-      final timeEntry = TimeEntry.forInsert(
-          taskId: taskId,
-          startTime: DateTime.now().subtract(const Duration(hours: 2)),
-          note: 'Worked on Task 1');
-      await DaoTimeEntry().insert(timeEntry);
-      timeEntry.copyWith(
-          endTime: timeEntry.startTime.add(const Duration(minutes: 60)));
-      await DaoTimeEntry().update(timeEntry);
-      final materialsBuy = await DaoCheckListItemType().getMaterialsBuy();
-
+      final now = DateTime.now();
+      final today = LocalDate.fromDateTime(now);
+      final job = await createJob(now, BillingType.timeAndMaterial,
+          hourlyRate: MoneyEx.dollars(50), callOutFee: MoneyEx.dollars(100));
+      final task = await createTask(job, 'Task 1');
       final checkList = await DaoCheckList().getByTask(task.id);
 
-      final completedMaterialItem = CheckListItem.forInsert(
-        checkListId: checkList!.id, // Assuming a check list ID
-        description: 'Completed Material Item',
-        itemTypeId: materialsBuy.id,
-        estimatedMaterialUnitCost: MoneyEx.fromInt(500), // $5/unit
-        estimatedMaterialQuantity: Fixed.fromNum(10, scale: 3), // 10 units
-        estimatedLabourHours: Fixed.fromNum(2, scale: 3), // 2 hours of labour
-        estimatedLabourCost: MoneyEx.fromInt(5000), // $50 labour cost
-        charge: MoneyEx.fromInt(5500), // $55 charge
-        margin: Percentage.fromNum(0.1, scale: 3), // 10% margin
-        completed: true,
-        measurementType: MeasurementType.length,
-        dimension1: Fixed.fromNum(1, scale: 3),
-        dimension2: Fixed.fromNum(1, scale: 3),
-        dimension3: Fixed.fromNum(1, scale: 3),
-        units: Units.m,
-        url: 'http://example.com/material',
-        labourEntryMode: LabourEntryMode.hours,
+      /// Item - Labour 2 hrs, $50 ph.
+      /// No effect on invoice as it is not billed
+      final labour = await insertLabourEstimates(
+        checkList,
+        MoneyEx.fromInt(5000), // $50 labour cost
+        Fixed.fromNum(2, scale: 3), // 2 hours of labour
       );
-      await DaoCheckListItem().insert(completedMaterialItem);
+
+      /// book time against the job - will be invoiced - $100
+      final timeEntry =
+          await createTimeEntry(task, now, const Duration(hours: 2));
 
       // Create invoice for the job
       final invoice =
-          await DaoInvoice().create(job, [taskId], groupByTask: false);
+          await DaoInvoice().create(job, [task.id], groupByTask: false);
 
       // Verify that the invoice includes both time entry and material item
       final invoiceLines = await DaoInvoiceLine().getByInvoiceId(invoice.id);
       expect(invoiceLines.length, equals(2));
 
+      final invoiceGroupLines =
+          await DaoInvoiceLineGroup().getByInvoiceId(invoice.id);
+      expect(invoiceGroupLines.length, equals(2));
+
+      expect(invoiceGroupLines[0].name, equals('Call Out Fee'));
+      expect(invoiceGroupLines[1].name, equals(formatLocalDate(today)));
       // Verify that the time entry and checklist item are marked as billed
       final billedTimeEntry = await DaoTimeEntry().getById(timeEntry.id);
       expect(billedTimeEntry?.billed, isTrue);
+      expect(
+          invoiceLines
+              .map((line) => line.id)
+              .contains(billedTimeEntry!.invoiceLineId),
+          isTrue);
 
-      final billedMaterialItem =
-          await DaoCheckListItem().getById(completedMaterialItem.id);
+      final billedMaterialItem = await DaoCheckListItem().getById(labour.id);
       expect(billedMaterialItem?.billed, isTrue);
+      expect(
+          invoiceLines
+              .map((line) => line.id)
+              .contains(billedMaterialItem!.invoiceLineId),
+          isTrue);
+
+      expect(invoice.totalAmount, MoneyEx.dollars(200));
     });
   });
 }
