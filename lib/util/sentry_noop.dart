2431a0bc4b5cd4bbc7d547a7d08cdb25ceaaa3f2
Brett Sutton <bsutton@onepub.dev>
2024-11-05 09:09:46 +1100

    dummy sentry file so we can conditionally use sentry on platforms that support it.

---
+++ lib/util/sentry_noop.dart
@@ -0,0 +1,9 @@
+/// Used to noop calls to sentry on non-supported platforms.
+///
+library;
+
+// ignore: avoid_classes_with_only_static_members
+class Sentry {
+  static Future<void> captureException(Object e,
+      {StackTrace? stackTrace}) async {}
+}
