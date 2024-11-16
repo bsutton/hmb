1860ba9e9e886579fa00f2921618734826250254
Brett Sutton <bsutton@noojee.com.au>
2024-11-16 15:40:44 +1100

    remove the call to canLaunchUrl as it has started failing on maps even the the call to launchUrl succeeds. https://github.com/flutter/flutter/issues/159014

--- lib/util/google_maps.dart
+++ lib/util/google_maps.dart
@@ -2,18 +2,19 @@ import 'package:flutter/widgets.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../entity/site.dart';
-import '../widgets/hmb_toast.dart';
 
 class GoogleMaps {
   GoogleMaps._();
 
   static Future<void> openMap(BuildContext context, Site site) async {
-    if (await canLaunchUrl(Uri.parse(site.toGoogleMapsQuery()))) {
-      await launchUrl(Uri.parse(site.toGoogleMapsQuery()));
-    } else {
-      if (context.mounted) {
-        HMBToast.error( 'Could not open the map.');
-      }
-    }
+    // if (await canLaunchUrl(Uri.parse(site.toGoogleMapsQuery()))) {
+    await launchUrl(Uri.parse(site.toGoogleMapsQuery()));
+    // }
+
+    // else {
+    //   if (context.mounted) {
+    //     HMBToast.error( 'Could not open the map.');
+    //   }
+    // }
   }
 }
