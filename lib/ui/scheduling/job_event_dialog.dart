365b77ddd1bb969deb2894960aa1f5b044af0e22
Brett Sutton <bsutton@noojee.com.au>
2025-01-01 18:15:07 +1100

    wip job_event_dialog

--- lib/ui/scheduling/job_event_dialog.dart
+++ lib/ui/scheduling/job_event_dialog.dart
@@ -1,61 +1,73 @@
+// job_event_dialog.dart
 import 'package:calendar_view/calendar_view.dart';
 import 'package:flutter/material.dart';
 
 import '../../dao/dao_job.dart';
 import '../../entity/job.dart';
+import '../../entity/job_event.dart';
 import '../widgets/hmb_button.dart';
 import '../widgets/hmb_date_time_picker.dart';
 import '../widgets/layout/hmb_spacer.dart';
 import '../widgets/select/hmb_droplist.dart';
 import 'schedule_page.dart';
 
+/// The dialog for adding/editing job events
 class JobEventDialog extends StatefulWidget {
-  JobEventDialog({
-    required CalendarEventData<JobEvent> this.event,
+  JobEventDialog.edit({
+    required CalendarEventData<JobEventEx> this.event,
     super.key,
-  }) : when = event.date;
+  })  : when = event.date,
+        isEditing = true;
 
-  const JobEventDialog.add({required this.when, super.key}) : event = null;
+  const JobEventDialog.add({
+    required this.when,
+    super.key,
+  })  : event = null,
+        isEditing = false;
 
-  // fields
-  final CalendarEventData<JobEvent>? event;
+  final CalendarEventData<JobEventEx>? event;
   final DateTime? when;
+  final bool isEditing;
 
   @override
   _JobEventDialogState createState() => _JobEventDialogState();
 
-  static Future<JobEvent?> showAdd(
-          {required BuildContext context, required DateTime when}) =>
-      showDialog<JobEvent>(
-          context: context,
-          builder: (context) =>
-              Material(child: JobEventDialog.add(when: when)));
+  static Future<JobEventEx?> showAdd({
+    required BuildContext context,
+    required DateTime when,
+  }) =>
+      showDialog<JobEventEx>(
+        context: context,
+        builder: (context) => Material(child: JobEventDialog.add(when: when)),
+      );
 
-  static Future<JobEvent?> showEdit(
-          BuildContext context, CalendarEventData<JobEvent> event) =>
-      showDialog<JobEvent>(
-          context: context,
-          builder: (context) => Material(child: JobEventDialog(event: event)));
+  static Future<JobEventEx?> showEdit(
+    BuildContext context,
+    CalendarEventData<JobEventEx> event,
+  ) =>
+      showDialog<JobEventEx>(
+        context: context,
+        builder: (context) =>
+            Material(child: JobEventDialog.edit(event: event)),
+      );
 }
 
 class _JobEventDialogState extends State<JobEventDialog> {
   late DateTime _startDate;
   late DateTime _endDate;
-
   Job? _selectedJob;
   final _form = GlobalKey<FormState>();
-
-  late final _titleNode = FocusNode();
-  late final _descriptionNode = FocusNode();
 
   @override
   void initState() {
     super.initState();
 
     if (widget.event == null) {
+      // Adding a new event
       _startDate = widget.when!;
       _endDate = widget.when!.add(const Duration(hours: 4));
     } else {
+      // Editing an existing event
       _startDate = widget.event!.startTime ?? DateTime.now();
       _endDate = widget.event!.endTime ?? DateTime.now();
       _selectedJob = widget.event?.event!.job;
@@ -63,105 +75,124 @@ class _JobEventDialogState extends State<JobEventDialog> {
   }
 
   @override
-  void dispose() {
-    _titleNode.dispose();
-    _descriptionNode.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) => Form(
         key: _form,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const SizedBox(height: 15),
-            HMBDroplist<Job>(
-              selectedItem: () async => _selectedJob,
-              items: (filter) async => DaoJob().getActiveJobs(filter),
-              format: (job) => job.summary,
-              onChanged: (job) => setState(() {
-                _selectedJob = job;
-              }),
-              title: 'Select Job',
-            ),
-            const SizedBox(height: 15),
-            Row(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
-                Expanded(
-                  child: HMBDateTimeField(
-                    showDate: false,
-                    label: 'Start Date',
-                    initialDateTime: _startDate,
-                    onChanged: (date) {
-                      // If new start date is after the current end date, shift end date
-                      if (date.isAfter(_endDate)) {
-                        _endDate = date.add(const Duration(hours: 1));
-                      }
-                      _startDate = date;
-                      setState(() {});
-                    },
-                  ),
+                HMBDroplist<Job>(
+                  selectedItem: () async => _selectedJob,
+                  items: (filter) async => DaoJob().getActiveJobs(filter),
+                  format: (job) => job.summary,
+                  onChanged: (job) => setState(() {
+                    _selectedJob = job;
+                  }),
+                  title: 'Select Job',
                 ),
-                const SizedBox(width: 20),
-                Expanded(
-                  child: HMBDateTimeField(
-                    showDate: false,
-                    initialDateTime: _endDate,
-                    label: 'End Date',
-                    onChanged: (date) {
-                      if (date.isBefore(_startDate)) {
-                        ScaffoldMessenger.of(context)
-                            .showSnackBar(const SnackBar(
-                          content: Text('End date occurs before start date.'),
-                        ));
-                      } else {
-                        _endDate = date;
-                      }
-                      setState(() {});
-                    },
-                  ),
+                const SizedBox(height: 15),
+                Row(
+                  children: [
+                    Expanded(
+                      child: HMBDateTimeField(
+                        showDate: false,
+                        label: 'Start Date',
+                        initialDateTime: _startDate,
+                        onChanged: (date) {
+                          if (date.isAfter(_endDate)) {
+                            _endDate = date.add(const Duration(hours: 1));
+                          }
+                          setState(() => _startDate = date);
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 15),
+                    Expanded(
+                      child: HMBDateTimeField(
+                        showDate: false,
+                        label: 'End Date',
+                        initialDateTime: _endDate,
+                        onChanged: (date) {
+                          if (date.isBefore(_startDate)) {
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(const SnackBar(
+                              content:
+                                  Text('End date occurs before start date.'),
+                            ));
+                            return;
+                          }
+                          setState(() => _endDate = date);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    HMBButtonSecondary(
+                      onPressed: () => Navigator.of(context).pop(),
+                      label: 'Cancel',
+                    ),
+                    const HMBSpacer(width: true),
+                    if (widget.isEditing) ...[
+                      HMBButtonSecondary(
+                        onPressed: _handleDelete,
+                        label: 'Delete',
+                      ),
+                      const HMBSpacer(width: true),
+                    ],
+                    HMBButtonPrimary(
+                      onPressed: _handleSave,
+                      label: widget.isEditing ? 'Update Event' : 'Add Event',
+                    ),
+                  ],
                 ),
               ],
             ),
-            const SizedBox(height: 15),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                HMBButtonPrimary(
-                  onPressed: () => Navigator.of(context).pop(),
-                  label: 'Cancel',
-                ),
-                const HMBSpacer(width: true),
-                HMBButtonPrimary(
-                  onPressed: _createEvent,
-                  label: widget.event == null ? 'Add Event' : 'Update Event',
-                ),
-              ],
-            ),
-          ],
+          ),
         ),
       );
 
-  void _createEvent() {
-    // If form or required fields fail validation, do nothing.
-    if (!(_form.currentState?.validate() ?? true)) return;
+  /// If the user taps “Delete,” we pop `null`
+  /// to indicate a delete request.
+  void _handleDelete() {
+    Navigator.of(context).pop();
+  }
 
-    // If no job is selected, ask the user to pick one.
+  Future<void> _handleSave() async {
+    if (!(_form.currentState?.validate() ?? true)) {
+      return;
+    }
+
     if (_selectedJob == null) {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text('Please select a job for the event.'),
-      ));
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Please select a job for the event.')),
+      );
       return;
     }
 
     _form.currentState?.save();
 
-    // Create a new JobEvent object with the selected job and date range
-    final jobEvent =
-        JobEvent(job: _selectedJob!, start: _startDate, end: _endDate);
+    late JobEvent jobEvent;
+    if (widget.isEditing) {
+      jobEvent = widget.event!.event!.jobEvent.copyWith(
+        jobId: _selectedJob!.id,
+        startDate: _startDate,
+        endDate: _endDate,
+      );
+    } else {
+      /// new job event.
+      jobEvent = JobEvent.forInsert(
+          jobId: _selectedJob!.id, startDate: _startDate, endDate: _endDate);
+    }
+    final jobEventEx = await JobEventEx.fromEvent(jobEvent);
 
-    // Return this event back to the caller
-    Navigator.of(context).pop(jobEvent);
+    if (mounted) {
+      Navigator.of(context).pop(jobEventEx);
+    }
   }
 }
