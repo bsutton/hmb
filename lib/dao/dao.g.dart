249060347e293de50e7d3ccd36f47350fdce8998
Brett Sutton <bsutton@noojee.com.au>
2025-02-08 13:21:07 +1100

    created barrell for dao.

---
+++ lib/dao/dao.g.dart
@@ -0,0 +1,46 @@
+//
+// Generated file. Do not modify.
+// Created by `barrel_create`
+// barrel_create is sponsored by OnePub the dart private repository
+// https://onepub.dev
+//
+export 'child_dao.dart';
+export 'dao.dart';
+export 'dao_base.dart';
+export 'dao_category.dart';
+export 'dao_check_list_item_check_list.dart';
+export 'dao_check_list_task.dart';
+export 'dao_contact.dart';
+export 'dao_contact_customer.dart';
+export 'dao_contact_supplier.dart';
+export 'dao_customer.dart';
+export 'dao_invoice.dart';
+export 'dao_invoice_create_by_date.dart';
+export 'dao_invoice_create_by_task.dart';
+export 'dao_invoice_fixed_price.dart';
+export 'dao_invoice_line.dart';
+export 'dao_invoice_line_group.dart';
+export 'dao_invoice_time_and_materials.dart';
+export 'dao_job.dart';
+export 'dao_job_activity.dart';
+export 'dao_job_status.dart';
+export 'dao_manufacturer.dart';
+export 'dao_message_template.dart';
+export 'dao_milestone.dart';
+export 'dao_photo.dart';
+export 'dao_quote.dart';
+export 'dao_quote_line.dart';
+export 'dao_quote_line_group.dart';
+export 'dao_site.dart';
+export 'dao_site_customer.dart';
+export 'dao_site_supplier.dart';
+export 'dao_supplier.dart';
+export 'dao_system.dart';
+export 'dao_task.dart';
+export 'dao_task_item.dart';
+export 'dao_task_item_type.dart';
+export 'dao_task_status.dart';
+export 'dao_time_entry.dart';
+export 'dao_tool.dart';
+export 'dao_version.dart';
+export 'database_extension.dart';
