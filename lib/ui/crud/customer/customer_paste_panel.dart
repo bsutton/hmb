769d20af803546c69bae66603fac2e4a351960d6
Brett Sutton <bsutton@onepub.dev>
2025-07-13 17:58:51 +1000

    added better email regex and support for extracting a mobile no.

--- lib/ui/crud/customer/customer_paste_panel.dart
+++ lib/ui/crud/customer/customer_paste_panel.dart
@@ -2,6 +2,7 @@ import 'package:deferred_state/deferred_state.dart';
 import 'package:flutter/material.dart';
 import 'package:strings/strings.dart';
 
+import '../../../util/parse.dart';
 import '../../../util/util.g.dart';
 import '../../widgets/fields/fields.g.dart';
 import '../../widgets/layout/layout.g.dart';
@@ -93,15 +94,13 @@ class ParsedCustomer {
     required this.email,
     required this.firstname,
     required this.surname,
+    required this.mobile,
     required this.address,
   });
 
   factory ParsedCustomer.parse(String text) {
-    const emailRegEx =
-        r'''(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])''';
-
-    final emailMatch = RegExp(emailRegEx).firstMatch(text);
-    final email = emailMatch?.group(0) ?? '';
+    final email = parseEmail(text);
+    final mobile = parsePhone(text);
 
     final nameMatch = RegExp(
       r'\b([A-Z][a-z]+)\s+([A-Z][a-z]+)\b',
@@ -124,13 +123,16 @@ class ParsedCustomer {
     return ParsedCustomer(
       customerName: customerName,
       email: email,
+      mobile: mobile,
       firstname: firstName,
       surname: surname,
       address: address,
     );
   }
+
   String customerName;
   String email;
+  String mobile;
   String firstname;
   String surname;
   ParsedAddress address;
@@ -139,6 +141,7 @@ class ParsedCustomer {
       Strings.isBlank(firstname) &&
       Strings.isBlank(surname) &&
       Strings.isBlank(email) &&
+      Strings.isBlank(mobile) &&
       Strings.isBlank(customerName) &&
       address.isEmpty();
 }
