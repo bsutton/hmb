dadefdaa9a1eeedcf2415b3e3b6940fa0a020035
Brett Sutton <bsutton@noojee.com.au>
2025-07-26 22:18:02 +1000

    lint

--- lib/ui/widgets/layout/hmb_padding.dart
+++ lib/ui/widgets/layout/hmb_padding.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
+
 import '../widgets.g.dart';
 
 class HMBPadding extends StatelessWidget {
-  const HMBPadding({
-    super.key,
-    required this.child,
-  });
+  const HMBPadding({required this.child, super.key});
 
   final Widget child;
 
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8),
-      child: Surface(child: child),
-    );
-  }
+  Widget build(BuildContext context) => Padding(
+    padding: const EdgeInsets.all(8),
+    child: Surface(child: child),
+  );
 }
