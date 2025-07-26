fe3a2738d07ecf20146c6362c9570a4bc48db33d
Brett Sutton <bsutton@noojee.com.au>
2025-07-26 22:11:03 +1000

    added widget to display full screen children.

---
+++ lib/ui/widgets/layout/hmb_full_page_child_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../text/text.g.dart';
+import 'hmb_padding.dart';
+
+class HMBFullPageChildScreen extends StatelessWidget {
+  const HMBFullPageChildScreen({
+    required this.child,
+    required this.title,
+    super.key,
+  });
+
+  final Widget child;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      leading: const BackButton(),
+      title: HMBTextHeadline(title),
+      backgroundColor: Colors.purple,
+    ),
+    body: HMBPadding(child: child),
+  );
+}
