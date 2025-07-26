11be1ae3a9a4958c7606d50bb40991ac8d5376ce
Brett Sutton <bsutton@noojee.com.au>
2025-07-26 22:15:58 +1000

    moved to new filter sheets.

--- lib/ui/crud/job/list_job_screen.dart
+++ lib/ui/crud/job/list_job_screen.dart
@@ -10,15 +10,13 @@
 */
 
 import 'package:flutter/material.dart';
-import 'package:june/june.dart';
 
 import '../../../dao/dao_job.dart';
 import '../../../dao/dao_job_status.dart';
 import '../../../entity/job.dart';
 import '../../../entity/job_status_enum.dart';
-import '../../widgets/hmb_button.dart';
-import '../../widgets/surface.dart';
 import '../../widgets/text/hmb_text_themes.dart';
+import '../../widgets/widgets.g.dart';
 import '../base_full_screen/list_entity_screen.dart';
 import 'edit_job_screen.dart';
 import 'job_card.dart';
@@ -34,50 +32,31 @@ class JobListScreen extends StatefulWidget {
 }
 
 class _JobListScreenState extends State<JobListScreen> {
+  var _showOldJobs = false;
+
   @override
   Widget build(BuildContext context) => Surface(
     elevation: SurfaceElevation.e0,
     child: Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            HMBButtonPrimary(
-              hint: June.getState(FilterState.new)._showOnHoldAndFinalised
-                  ? 'Show active Jobs'
-                  : 'Show Jobs that are on hold or have been finalised.',
-              onPressed: () {
-                setState(() {
-                  June.getState(FilterState.new).toggle();
-                });
-              },
-              label: June.getState(FilterState.new)._showOnHoldAndFinalised
-                  ? 'Show PreStart & Progressing'
-                  : 'Show OnHold & Finalised',
-            ),
-          ],
-        ),
         Flexible(
-          child: JuneBuilder(
-            FilterState.new,
-            builder: (context) => EntityListScreen<Job>(
-              key: ValueKey(
-                June.getState(FilterState.new)._showOnHoldAndFinalised,
-              ),
-              dao: DaoJob(),
-              pageTitle: JobListScreen.pageTitle,
-              onEdit: (job) => JobEditScreen(job: job),
-              fetchList: _fetchJobs,
-              title: (job) => HMBCardTitle(job.summary),
-              cardHeight: 700,
-              background: (job) async =>
-                  (await DaoJobStatus().getById(
-                    job.jobStatusId,
-                  ))?.getColour() ??
-                  Colors.green,
-              details: (job) => JobCard(job: job, key: ValueKey(job.hashCode)),
-            ),
+          child: EntityListScreen<Job>(
+            dao: DaoJob(),
+            pageTitle: JobListScreen.pageTitle,
+            onEdit: (job) => JobEditScreen(job: job),
+            fetchList: _fetchJobs,
+            title: (job) => HMBCardTitle(job.summary),
+            cardHeight: 700,
+            filterSheetBuilder: _buildFilterSheet,
+            isFilterActive: () => _showOldJobs,
+            onFiltersCleared: () {
+              _showOldJobs = false;
+            },
+            background: (job) async =>
+                (await DaoJobStatus().getById(job.jobStatusId))?.getColour() ??
+                Colors.green,
+            details: (job) => JobCard(job: job, key: ValueKey(job.hashCode)),
           ),
         ),
       ],
@@ -90,7 +69,7 @@ class _JobListScreenState extends State<JobListScreen> {
     for (final job in jobs) {
       final jobStatus = await DaoJobStatus().getById(job.jobStatusId);
       final status = jobStatus?.statusEnum;
-      if (June.getState(FilterState.new)._showOnHoldAndFinalised) {
+      if (_showOldJobs) {
         if (status == JobStatusEnum.onHold ||
             status == JobStatusEnum.finalised) {
           selected.add(job);
@@ -104,13 +83,28 @@ class _JobListScreenState extends State<JobListScreen> {
     }
     return selected;
   }
+
+  Widget _buildFilterSheet(void Function() onChange) =>
+      SwitchListTile(
+        title: const Text('Show only Old Jobs'),
+        value: _showOldJobs,
+        onChanged: (val) {
+          setState(() {
+            _showOldJobs = val;
+          });
+          onChange();
+        },
+      ).help(
+        'Show only Old Jobs',
+        'Only show Jobs that are on hold or have been finalised.',
+      );
 }
 
-class FilterState extends JuneState {
-  var _showOnHoldAndFinalised = false;
+// class FilterState extends JuneState {
+//   var _showOnHoldAndFinalised = false;
 
-  void toggle() {
-    _showOnHoldAndFinalised = !_showOnHoldAndFinalised;
-    setState();
-  }
-}
+//   void toggle() {
+//     _showOnHoldAndFinalised = !_showOnHoldAndFinalised;
+//     setState();
+//   }
+// }
