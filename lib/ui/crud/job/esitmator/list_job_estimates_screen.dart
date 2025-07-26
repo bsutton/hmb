29f712304c1237b3c698161a1e0135d63348f773
Brett Sutton <bsutton@noojee.com.au>
2025-07-26 22:15:39 +1000

    moved to using an EntityList

--- lib/ui/crud/job/esitmator/list_job_estimates_screen.dart
+++ lib/ui/crud/job/esitmator/list_job_estimates_screen.dart
@@ -9,183 +9,115 @@
  https://github.com/bsutton/hmb/blob/main/LICENSE
 */
 
-import 'package:deferred_state/deferred_state.dart';
 import 'package:flutter/material.dart';
-import 'package:future_builder_ex/future_builder_ex.dart';
 
 import '../../../../dao/dao.g.dart';
 import '../../../../entity/entity.g.dart';
 import '../../../../util/app_title.dart';
-import '../../../widgets/hmb_search.dart';
-import '../../../widgets/hmb_toggle.dart';
 import '../../../widgets/layout/hmb_spacer.dart';
-import 'job_card.dart'; // The JobCard from previous snippet
+import '../../../widgets/text/text.g.dart';
+import '../../base_full_screen/base_full_screen.g.dart';
+import 'job_card.dart';
 
 class JobEstimatesListScreen extends StatefulWidget {
   const JobEstimatesListScreen({super.key, this.job});
 
   final Job? job;
+
   @override
-  _JobEstimatesListScreenState createState() => _JobEstimatesListScreenState();
+  State<JobEstimatesListScreen> createState() => _JobEstimatesListScreenState();
 }
 
-class _JobEstimatesListScreenState
-    extends DeferredState<JobEstimatesListScreen> {
-  late Future<List<CustomerAndJob>> _jobs;
-  var _showOnlyActiveJobs = true;
-
-  var _searchQuery = '';
+class _JobEstimatesListScreenState extends State<JobEstimatesListScreen> {
+  var _onlyShowQutableJobs = true;
 
   @override
-  Future<void> asyncInitState() async {
+  void initState() {
+    super.initState();
     setAppTitle('Estimator');
-    await _refreshJobs();
   }
 
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  void _onSearchChanged(String? filter) {
-    setState(() {
-      _searchQuery = filter?.trim().toLowerCase() ?? '';
-    });
-  }
-
-  Future<void> _refreshJobs() async {
-    _jobs = _fetchJobs();
-    setState(() {});
-  }
-
-  Future<List<CustomerAndJob>> _fetchJobs() async {
+  Future<List<Job>> _fetchFiltered(String? filter) async {
     List<Job> rawJobs;
-    if (_showOnlyActiveJobs) {
-      rawJobs = await DaoJob().getQuotableJobs(null);
+    if (_onlyShowQutableJobs) {
+      rawJobs = await DaoJob().getQuotableJobs(filter);
     } else {
-      rawJobs = await DaoJob().getByFilter(null);
+      rawJobs = await DaoJob().getByFilter(filter);
     }
 
-    final jobList = <CustomerAndJob>[];
+    final jobList = <Job>[];
     for (final job in rawJobs) {
       final customer = await DaoCustomer().getByJob(job.id);
       if (customer == null) {
         continue;
       }
 
-      final contact = await DaoContact().getPrimaryForJob(job.id);
-      final hasBillables = await _hasBillableItems(job);
-      jobList.add(
-        CustomerAndJob(
-          customer: customer,
-          job: job,
-          hasBillables: hasBillables,
-          contactName: contact?.fullname,
-        ),
-      );
+      // final contact = await DaoContact().getPrimaryForJob(job.id);
+
+      // final hasBillables =
+      //     await DaoJob().hasBillableTasks(job) ||
+      //     await DaoJob().hasBillableBookingFee(job);
+      // if (_onlyBillables && !hasBillables) {
+      //   continue;
+      // }
+
+      // final cj = CustomerAndJob(
+      //   customer: customer,
+      //   job: job,
+      //   hasBillables: hasBillables,
+      //   contactName: contact?.fullname,
+      // );
+      jobList.add(job);
     }
 
     return jobList;
   }
 
-  Future<bool> _hasBillableItems(Job job) async {
-    final hasBillableTasks = await DaoJob().hasBillableTasks(job);
-    final hasBillableBookingFee = await DaoJob().hasBillableBookingFee(job);
-    return hasBillableTasks || hasBillableBookingFee;
-  }
-
-  List<CustomerAndJob> _filterJobs(List<CustomerAndJob> jobs) {
-    if (_searchQuery.isEmpty) {
-      return jobs;
-    }
-    return jobs.where((cj) {
-      final customerName = cj.customer.name.toLowerCase();
-      final jobSummary = cj.job.summary.toLowerCase();
-      final contactName = (cj.contactName ?? '').toLowerCase();
-
-      return customerName.contains(_searchQuery) ||
-          jobSummary.contains(_searchQuery) ||
-          contactName.contains(_searchQuery);
-    }).toList();
-  }
-
-  final double _searchBarRowHeight = 80; // Adjust as needed for your search bar
-  final double _switchRowHeight = 50; //
-
   @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      automaticallyImplyLeading: false,
-      toolbarHeight: _searchBarRowHeight + _switchRowHeight,
-      title: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: HMBSearch(
-                  onSearch: (filter) async => _onSearchChanged(filter),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            // New Row for the Switch
-            mainAxisAlignment: MainAxisAlignment.end, // Align to the right
-            children: [
-              HMBToggle(
-                label: 'Show All Jobs',
-                initialValue: !_showOnlyActiveJobs,
-                onToggled: (value) async {
-                  setState(() {
-                    _showOnlyActiveJobs = !value;
-                  });
-                  await _refreshJobs();
-                },
-                tooltip: '',
-              ),
-            ],
-          ),
-        ],
-      ),
-    ),
-    body: Column(
+  Widget build(BuildContext context) => EntityListScreen<Job>(
+    pageTitle: 'Estimator',
+    dao: DaoJob(), // dummy DAO just to meet API, not used
+    fetchList: _fetchFiltered,
+    showBackButton: widget.job != null,
+    title: (job) async =>
+        HMBTextHeadline((await _getCustomer(job))?.name ?? 'Unknown'),
+    details: (job) => Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Expanded(
-          child: FutureBuilderEx<List<CustomerAndJob>>(
-            future: _jobs,
-            builder: (context, jobs) {
-              if (jobs == null || jobs.isEmpty) {
-                return const Center(
-                  child: Text(
-                    'No jobs found. Only Jobs with a status of Quoting or Prospecting are shown',
-                  ),
-                );
-              }
-
-              final filteredJobs = _filterJobs(jobs);
-              if (filteredJobs.isEmpty) {
-                return const Center(child: Text('No matches for your search.'));
-              }
-
-              return ListView.builder(
-                itemCount: filteredJobs.length,
-                itemBuilder: (context, index) {
-                  final cj = filteredJobs[index];
-                  return Column(
-                    children: [
-                      const HMBSpacer(height: true),
-                      JobCard(job: cj.job, onEstimatesUpdated: _refreshJobs),
-                    ],
-                  );
-                },
-              );
-            },
-          ),
-        ),
+        const HMBSpacer(height: true),
+        JobCard(job: job, onEstimatesUpdated: () => setState(() {})),
       ],
     ),
+    cardHeight: 370,
+    filterSheetBuilder: _buildFilterSheet,
+    isFilterActive: () => _onlyShowQutableJobs,
+    onFiltersCleared: () {
+      _onlyShowQutableJobs = false;
+    },
+    canAdd: false,
+    onEdit: (job) => JobCard(
+      job: job!,
+      onEstimatesUpdated: () => setState(() {}),
+    ), // no edit screen
+  );
+
+  Widget _buildFilterSheet(void Function() onChange) => Column(
+    children: [
+      SwitchListTile(
+        title: const Text('Only Show Quotable Jobs'),
+        value: _onlyShowQutableJobs,
+        onChanged: (val) {
+          setState(() {
+            _onlyShowQutableJobs = val;
+          });
+          onChange();
+        },
+      ),
+    ],
   );
 }
+
+Future<Customer?> _getCustomer(Job job) async => DaoCustomer().getByJob(job.id);
 
 class CustomerAndJob {
   CustomerAndJob({
