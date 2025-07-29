b073e2acdf0fa8b44b574b78d5112e1ab10876ef
Brett Sutton <bsutton@noojee.com.au>
2025-07-29 18:11:19 +1000

    removed padding for consistency.

--- lib/ui/widgets/layout/hmb_full_page_child_screen.dart
+++ lib/ui/widgets/layout/hmb_full_page_child_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../text/text.g.dart';
-import 'hmb_padding.dart';
 
 class HMBFullPageChildScreen extends StatelessWidget {
   const HMBFullPageChildScreen({
@@ -20,6 +19,6 @@ class HMBFullPageChildScreen extends StatelessWidget {
       title: HMBTextHeadline(title),
       backgroundColor: Colors.purple,
     ),
-    body: HMBPadding(child: child),
+    body: child,
   );
 }
