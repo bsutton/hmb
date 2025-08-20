0a0dbbbfaaa65f07248c57d6ce0744d1f551f8d9
Brett Sutton <bsutton@noojee.com.au>
2025-08-21 08:03:57 +1000

    moved todo list card into its own widget. Move the due date to its own line to provide more space for the title.

---
+++ lib/ui/crud/todo/list_todo_card.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:strings/strings.dart';
+
+import '../../../dao/dao_todo.dart';
+import '../../../entity/todo.dart';
+import '../../../util/format.dart';
+import '../../dialog/hmb_snooze_picker.dart';
+import '../../widgets/layout/layout.g.dart';
+import '../../widgets/select/hmb_entity_chip.dart';
+import '../../widgets/text/text.g.dart';
+import '../../widgets/widgets.g.dart';
+import 'list_todo_screen.dart';
+
+class ListTodoCard extends StatelessWidget {
+  const ListTodoCard({required this.todo, super.key});
+
+  final ToDo todo;
+
+  @override
+  Widget build(BuildContext context) => HMBColumn(
+    children: [
+      HMBRow(
+        children: [
+          if (todo.parentType == null) const HMBChip(label: 'Personal'),
+          if (todo.parentType == ToDoParentType.job)
+            HMBEntityChip.job(id: todo.parentId!, format: (job) => job.summary),
+          if (todo.parentType == ToDoParentType.customer)
+            HMBEntityChip.customer(
+              id: todo.parentId!,
+              format: (customer) => customer.name,
+            ),
+          HMBChip(label: 'Priority: ${todo.priority.name}'),
+          HMBMenuChip<SnoozeOption>(
+            label: 'Snooze',
+            icon: Icons.snooze,
+            tone: HMBChipTone.accent,
+            values: SnoozeOption.values,
+            format: (o) => o.description,
+            itemIcon: (o) => o.icon,
+            onSelected: (o) async {
+              var duration = o.duration;
+              if (duration == null) {
+                // For "Pick…": compute base (same logic your DAO uses)
+                final base = todo.dueDate ?? DateTime.now();
+                duration = await HMBSnoozePicker.pickSnoozeDuration(
+                  context,
+                  base: base,
+                  initial: base.add(const Duration(hours: 2)),
+                );
+              }
+
+              if (duration != null) {
+                await DaoToDo().snooze(todo, o.duration!);
+              }
+            },
+          ),
+        ],
+      ),
+      if (todo.parentType == ToDoParentType.job)
+        HMBButton.small(
+          label: 'Convert to Task',
+          hint: 'Covert the todo item into a Job Task',
+          onPressed: () => DaoToDo().convertToTask(todo),
+        ),
+      if (todo.dueDate != null)
+        HMBChip(
+          label: formatDue(todo.dueDate!), // Overdue/Today/Thu 09:00
+          tone: dueTone(todo.dueDate), // red/purple/neutral
+        ),
+      if (Strings.isNotBlank(todo.note)) HMBTextBlock(todo.note!, maxLines: 4),
+    ],
+  );
+}
--- lib/ui/crud/todo/list_todo_screen.dart
+++ lib/ui/crud/todo/list_todo_screen.dart
@@ -3,16 +3,11 @@ import 'package:flutter/material.dart';
 import '../../../dao/dao_todo.dart';
 import '../../../entity/todo.dart';
 import '../../../util/util.g.dart';
-import '../../dialog/hmb_snooze_picker.dart';
-import '../../widgets/hmb_chip.dart';
-import '../../widgets/hmb_menu_chip.dart';
-import '../../widgets/hmb_select_chips.dart';
-import '../../widgets/layout/hmb_spacer.dart';
-import '../../widgets/select/hmb_entity_chip.dart';
 import '../../widgets/text/text.g.dart';
 import '../../widgets/widgets.g.dart';
 import '../base_full_screen/list_entity_screen.dart';
 import 'edit_todo_screen.dart';
+import 'list_todo_card.dart';
 
 class ToDoListScreen extends StatefulWidget {
   const ToDoListScreen({super.key});
@@ -45,70 +40,12 @@ class _ToDoListScreenState extends State<ToDoListScreen> {
           },
         ),
         Expanded(child: HMBTextHeadline2(t.title)),
-        if (t.dueDate != null)
-          HMBChip(
-            label: formatDue(t.dueDate!), // Overdue/Today/Thu 09:00
-            tone: dueTone(t.dueDate), // red/purple/neutral
-          ),
       ],
     ),
     background: (t) async => t.status == ToDoStatus.open && isOverdue(t.dueDate)
         ? SurfaceElevation.e6.color.withSafeOpacity(0.92)
         : SurfaceElevation.e6.color,
-    details: (t) => _buildCard(t, context),
-  );
-
-  Column _buildCard(ToDo t, BuildContext context) => Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      if (t.note != null) HMBTextBlock(t.note!, maxLines: 4),
-      const HMBSpacer(height: true),
-      Wrap(
-        spacing: 8,
-        runSpacing: 8,
-        children: [
-          if (t.parentType == null) const HMBChip(label: 'Personal'),
-          if (t.parentType == ToDoParentType.job)
-            HMBEntityChip.job(id: t.parentId!, format: (job) => job.summary),
-          if (t.parentType == ToDoParentType.customer)
-            HMBEntityChip.customer(
-              id: t.parentId!,
-              format: (customer) => customer.name,
-            ),
-          HMBChip(label: 'Priority: ${t.priority.name}'),
-          HMBMenuChip<SnoozeOption>(
-            label: 'Snooze',
-            icon: Icons.snooze,
-            tone: HMBChipTone.accent,
-            values: SnoozeOption.values,
-            format: (o) => o.description,
-            itemIcon: (o) => o.icon,
-            onSelected: (o) async {
-              var duration = o.duration;
-              if (duration == null) {
-                // For "Pick…": compute base (same logic your DAO uses)
-                final base = t.dueDate ?? DateTime.now();
-                duration = await HMBSnoozePicker.pickSnoozeDuration(
-                  context,
-                  base: base,
-                  initial: base.add(const Duration(hours: 2)),
-                );
-              }
-
-              if (duration != null) {
-                await DaoToDo().snooze(t, o.duration!);
-              }
-            },
-          ),
-          if (t.parentType == ToDoParentType.job)
-            HMBButton.small(
-              label: 'Convert to Task',
-              hint: 'Covert the todo item into a Job Task',
-              onPressed: () => DaoToDo().convertToTask(t),
-            ),
-        ],
-      ),
-    ],
+    details: (t) => ListTodoCard(todo: t),
   );
 
   Widget _buildFilterSheet(BuildContext context, void Function() onChange) =>
