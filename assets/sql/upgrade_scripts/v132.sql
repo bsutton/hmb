89061cc429324b1517f51fe24d3b03a41b79a3a3
Brett Sutton <bsutton@noojee.com.au>
2025-09-03 20:21:00 +1000

    formatting.

--- assets/sql/upgrade_scripts/v132.sql
+++ assets/sql/upgrade_scripts/v132.sql
@@ -1,4 +1,5 @@
 -- fixed records wth null actuals caused by flawed forUpdate method.
+
 update task_item
 set actual_material_unit_cost = estimated_material_unit_cost,
 actual_material_quantity = estimated_material_quantity
@@ -6,6 +7,5 @@ actual_material_quantity = estimated_material_quantity
 where actual_material_unit_cost is NULL
 and charge is not null
 and charge_set = 1
-and labour_entry_mode = 'Dollars'
-;
+and labour_entry_mode = 'Dollars';
 
