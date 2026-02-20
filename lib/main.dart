e25b641f037bd64ccb04b1b70e371c28d2ce08b3
Brett Sutton <bsutton@noojee.com.au>
2026-02-20 16:33:50 +1100

    uprade to new capatino import location.

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@
 import 'dart:async';
 
 import 'package:device_preview_plus/device_preview_plus.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:june/june.dart';
