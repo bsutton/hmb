7b3e5a2d90b4e63915d385bfda9aaef326ac7e8b
Brett Sutton <bsutton@noojee.com.au>
2026-02-24 12:56:42 +1100

    Fix #165: mark job active when opening full-page card

--- lib/ui/crud/job/full_page_list_job_card.dart
+++ lib/ui/crud/job/full_page_list_job_card.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 
+import '../../../dao/dao_job.dart';
 import '../../../entity/job.dart';
 import '../../widgets/layout/hmb_full_page_child_screen.dart';
 import 'list_job_card.dart';
 
-class FullPageListJobCard extends StatelessWidget {
+class FullPageListJobCard extends StatefulWidget {
   final Job job;
 
   const FullPageListJobCard(this.job, {super.key});
 
   @override
+  State<FullPageListJobCard> createState() => _FullPageListJobCardState();
+}
+
+class _FullPageListJobCardState extends State<FullPageListJobCard> {
+  @override
+  void initState() {
+    super.initState();
+    // Opening a job details card should make that job the active job.
+    Future<void>(() => DaoJob().markActive(widget.job.id));
+  }
+
+  @override
   Widget build(BuildContext context) => HMBFullPageChildScreen(
     title: 'Job',
-    child: ListJobCard(job: job),
+    child: ListJobCard(job: widget.job),
   );
 }
