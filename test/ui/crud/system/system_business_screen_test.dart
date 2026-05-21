f5c9ce74dab9b85d32caaef8e4af6778fd450dac
Brett Sutton <bsutton@noojee.com.au>
2026-05-21 23:13:37 +1000

    Fix #511: pad business details form

---
+++ test/ui/crud/system/system_business_screen_test.dart
@@ -0,0 +1,27 @@
+@Tags(['flutter'])
+library;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hmb/ui/crud/system/system_business_screen.dart';
+
+import '../../../database/management/db_utility_test_helper.dart';
+
+void main() {
+  setUp(() async {
+    await setupTestDb();
+  });
+
+  tearDown(() async {
+    await tearDownTestDb();
+  });
+
+  testWidgets('business details form has screen edge padding', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: SystemBusinessScreen()));
+    await tester.pumpAndSettle();
+
+    final listView = tester.widget<ListView>(find.byType(ListView));
+
+    expect(listView.padding, const EdgeInsets.all(16));
+  });
+}
