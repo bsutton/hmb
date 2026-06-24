a65b16da570e37191b4c5f2dcd81494e2abbaa02
Brett Sutton <bsutton@noojee.com.au>
2026-06-24 22:20:14 +1000

    Fix #537: exclude completed tasks from estimates

--- lib/dao/dao_task.dart
+++ lib/dao/dao_task.dart
@@ -214,7 +214,7 @@ WHERE ti.id = ?
       final hourlyRate = await DaoTask().getHourlyRate(task);
       final taskStatus = task.status;
 
-      if (!taskStatus.isWithdrawn()) {
+      if (!taskStatus.isWithdrawn() && !taskStatus.isComplete()) {
         final estimate = await getEstimateForTask(job, task, hourlyRate);
         if (!estimate.total.isZero) {
           estimates.add(estimate);
