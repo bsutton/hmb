9834f7aa9e08ccc3da74e1441b251f92973ab699
Brett Sutton <bsutton@noojee.com.au>
2026-08-11 16:28:40 +1000

    Fix #562: test photo sync resume after sign-in

--- test/database/management/backup_providers/google_drive/background_backup/photo_sync_service_test.dart
+++ test/database/management/backup_providers/google_drive/background_backup/photo_sync_service_test.dart
@@ -2,10 +2,73 @@
 // ignore_for_file: lines_longer_than_80_chars
 library;
 
+import 'dart:async';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hmb/database/management/backup_providers/google_drive/background_backup/photo_sync_service.dart';
 
 void main() {
+  group('photo sync sign-in resume', () {
+    late StreamController<bool> authStateChanges;
+    late StreamSubscription<bool> subscription;
+    var waitingForSignIn = false;
+    var running = false;
+    var startCalls = 0;
+    var waitingWhenStarted = true;
+
+    setUp(() {
+      waitingForSignIn = false;
+      running = false;
+      startCalls = 0;
+      waitingWhenStarted = true;
+      authStateChanges = StreamController<bool>.broadcast(sync: true);
+      subscription = listenForPhotoSyncSignIn(
+        authStateChanges: authStateChanges.stream,
+        isWaitingForSignIn: () => waitingForSignIn,
+        isRunning: () => running,
+        clearWaitingForSignIn: () => waitingForSignIn = false,
+        start: () async {
+          startCalls++;
+          waitingWhenStarted = waitingForSignIn;
+        },
+      );
+    });
+
+    tearDown(() async {
+      await subscription.cancel();
+      await authStateChanges.close();
+    });
+
+    test('starts once when sign-in completes while sync is waiting', () {
+      waitingForSignIn = true;
+
+      authStateChanges
+        ..add(true)
+        ..add(true);
+
+      expect(startCalls, 1);
+      expect(waitingForSignIn, isFalse);
+      expect(waitingWhenStarted, isFalse);
+    });
+
+    test('ignores auth events when resume conditions are not met', () {
+      waitingForSignIn = true;
+      authStateChanges.add(false);
+      expect(startCalls, 0);
+      expect(waitingForSignIn, isTrue);
+
+      running = true;
+      authStateChanges.add(true);
+      expect(startCalls, 0);
+      expect(waitingForSignIn, isTrue);
+
+      running = false;
+      waitingForSignIn = false;
+      authStateChanges.add(true);
+      expect(startCalls, 0);
+    });
+  });
+
   group('isRecoverablePhotoSyncError', () {
     test('matches transient network and sleep interruption messages', () {
       expect(
