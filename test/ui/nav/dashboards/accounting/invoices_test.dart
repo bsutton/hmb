87ee1a55e08d02221e02c1195822fcc68a8767dc
Brett Sutton <bsutton@noojee.com.au>
2026-09-20 22:49:44 +1000

    Clarify non-overlapping overdue invoice groups (#616)

--- test/ui/nav/dashboards/accounting/invoices_test.dart
+++ test/ui/nav/dashboards/accounting/invoices_test.dart
@@ -108,7 +108,7 @@ void main() {
     );
 
     expect(find.text('12 unpaid invoices'), findsOneWidget);
-    expect(find.text('5 of these overdue'), findsOneWidget);
+    expect(find.text('3 overdue, 2 7+ days'), findsOneWidget);
     expect(find.text('5 overdue, 2 7+ days'), findsNothing);
     expect(find.textContaining('34'), findsNothing);
     expect(tester.takeException(), isNull);
